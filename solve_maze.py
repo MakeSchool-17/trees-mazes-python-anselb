23809f7e6eb22f7157f23a232ab4012d3a58f200
Ansel Bridgewater <iamansel@gmail.com>
2018-05-06 23:34:09 -0700

    Finished solve_dfs function for solving maze

--- solve_maze.py
+++ solve_maze.py
@@ -1,13 +1,39 @@
 import maze
 import generate_maze
 import sys
+import random
 
 
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
 def solve_dfs(m):
-    # TODO: Implement solve_dfs
-    pass
+    # Implement solve_dfs
+    cells_to_backtrack = []
+    current_cell = 0
+    visited_cells = 0
 
+    # Search for solution while the current cell is not at the solution cell
+    while current_cell != m.total_cells - 1:
+        unvisited_neighbors = m.cell_neighbors(current_cell)
+        if len(unvisited_neighbors) >= 1:
+            # Choose a random neighbor and separate the tuple
+            new_cell_tuple = random.choice(unvisited_neighbors)
+            new_cell = new_cell_tuple[0]
+            direction = new_cell_tuple[1]
+
+            # Visit the new cell
+            m.visit_cell(current_cell, new_cell, direction)
+            # Append the cell to stack in case it needs to be backtracked later
+            cells_to_backtrack.append(current_cell)
+            # Move to the neighbor cell
+            current_cell = new_cell
+            visited_cells += 1
+        else:
+            # Blank out solution bits so it is no longer on the solution path
+            m.backtrack(current_cell)
+            current_cell = cells_to_backtrack.pop()
+        m.refresh_maze_view()
+    print('Maze solved')
+    m.state = 'idle'
 
 # Solve maze using BFS algorithm, terminate with solution
 def solve_bfs(m):
